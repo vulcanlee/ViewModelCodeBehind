d459803ab9411b13b46973c279593f4437341c37
李進興 Vulcan <vulcan@miniasp.com>
2017-07-28 13:18:42 +0800

    XFCodeBehind 專案，使用 Code Behind 來變更顏色資源的顏色

--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 
 在這裡，將會展示如何從原先的 Code Behind 相關處理程式碼，變更或者包裝成為 ViewModel 可以呼叫這些功能。
 
-## XFCodeBehind
+## XFCodeBehind 專案，使用 Code Behind 來變更顏色資源的顏色
 
 這個專案將會說明，如何使用 Code Behind，當使用按下按鈕之後，使用動態資源的方式，變更 ContentPage 的背景顏色
 
+下圖為該範例程式執行的螢幕截圖
+
+![Xamarin Code Behind](Screenshot/CodeBehind1.png)
+
+下圖為當按下的 `變更顏色` 按鈕之後，頁面背景顏色會變色的螢幕截圖
+
+![Xamarin Code Behind](Screenshot/CodeBehind2.png)
+
+下圖為當按下的 `變更顏色(頁面)` 按鈕之後，該按鈕背景顏色會變色的螢幕截圖
+
+![Xamarin Code Behind](Screenshot/CodeBehind3.png)
---
+++ XFCodeBehind/XFCodeBehind/XFCodeBehind/Views/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XFCodeBehind.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeColor_Clicked(object sender, System.EventArgs e)
+        {
+            ChangePageColor();
+        }
+
+        private void ChangePageColor()
+        {
+            // 按下按鈕，將會即時變更背影顏色 (App.Current.Resources 是 VisualElement 內的一個屬性，因此，只能使用 Code Behind 才能夠存取這個屬性 )
+            // 為什麼呢？ 因為 ViewModel 並不知道真正要使用的 View 是哪個，所以，也無從存取任何 View 中的控制項
+            App.Current.Resources["DynamicResourceColor"] = Color.FromHex("#768ddb");
+        }
+
+        private void btnPageChangeColor_Clicked(object sender, System.EventArgs e)
+        {
+            ChangeButtonColor();
+        }
+
+        private void ChangeButtonColor()
+        {
+            this.Resources["PageDynamicResourceColor"] = Color.FromHex("#e89a4c");
+        }
+    }
+}
